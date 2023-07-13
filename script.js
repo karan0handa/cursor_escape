809e77a7891585322585b447307288d44c7e5676
Karan Handa <karan.handa_ug23@ashoka.edu.in>
2023-07-13 13:52:05 +0530

    Basic Box

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * This is a game where there is a ball chasing the cursor. The moment that get hits the cursor, game over.
+ */
+
+function mouseOverBox() {
+  alert('You Lost!')
+}
+
+function getPointerPosition() {
+
+}
+
